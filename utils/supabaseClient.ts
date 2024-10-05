d1d34eef79f72a3a72e95d05a7438d7852e04cbd
zmercury <messiniki@outlook.com>
2024-10-05 10:24:28 +0545

    📟 Added Instructions on DB

--- utils/supabaseClient.ts
+++ utils/supabaseClient.ts
@@ -1,5 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
+//create an environment variable for the supabase url and the anon key on .env file
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
 
